9b6c8e8ff21739e23d8261a7526193b57f4ce733
Ian <ian.a.wiggins919@gmail.com>
2016-09-06 15:42:29 -0600

    initial commit

--- xlstTest/Program.cs
+++ xlstTest/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Xsl;
 
 namespace xlstTest
 {
@@ -10,6 +12,21 @@ namespace xlstTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Loading Transform 0");
+            XslCompiledTransform transform0 = new XslCompiledTransform();
+            transform0.Load("variant0Transform.xslt");
+            Console.WriteLine("Transforming 0");
+            transform0.Transform("variant0.xml","out0.xml");
+            Console.WriteLine("0 Done");
+
+            Console.WriteLine("Loading Transform 1");
+            XslCompiledTransform transform1 = new XslCompiledTransform();
+            transform1.Load("variant1Transform.xslt");
+            Console.WriteLine("Transforming 1");
+            transform1.Transform("variant1.xml","out1.xml");
+            Console.WriteLine("1 Done");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ xlstTest/variant1Transform.xslt
@@ -0,0 +1,21 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl"
+>
+    <xsl:output method="xml" indent="yes"/>
+    <xsl:template match="/">
+        <books>
+            <xsl:for-each select="books/book">
+                    <book>
+                        <author>
+                            <xsl:value-of select="@author"/>
+                        </author>
+                        <title>
+                            <xsl:value-of select="@title"/>
+                        </title>
+                    </book>
+            </xsl:for-each>
+        </books>
+    </xsl:template>
+    
+</xsl:stylesheet>
